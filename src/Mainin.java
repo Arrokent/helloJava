89b3cc4449c154a1db80cadd277b5a00c1d83d20
tianyao <wtmsbao@163.com>
2021-06-01 20:48:51 +0800

    Initial commit

---
+++ src/Mainin.java
@@ -0,0 +1,15 @@
+import java.util.function.*;
+import java.util.*;
+
+public class Mainin {
+    private static String getString(Supplier<String> function) {
+        return function.get();
+    }
+
+    public static void main(String[] args) {
+        String msgA = "Hello";
+        String msgB = "World";
+        System.out.println(getString(() -> msgA + msgB));
+    }
+
+}
